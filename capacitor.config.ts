0b360802b6eb01b9639031af32ccb3b973c924ec
arpho <damicogiuseppe77@gmail.com>
2024-07-30 19:02:04 +0200

    initialized the template with firestore init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wodRecord',
+  webDir: 'www'
+};
+
+export default config;
