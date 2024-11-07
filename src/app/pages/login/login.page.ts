900150cfe5b3a9f6c128c05945af474ac3cdf403
arpho <damicogiuseppe77@gmail.com>
2024-11-07 13:13:37 +0100

    login form wip

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,33 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, 
+  imports: [IonInput, IonLabel, IonItem, IonContent, 
     IonHeader, 
     IonTitle, 
     IonToolbar, 
+    IonLabel,
     CommonModule, 
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+  
   ]
 })
 export class LoginPage implements OnInit {
 loginForm: FormGroup 
+email="";
+password="";
   constructor(
     private fb: FormBuilder
-  ) { }
-
-  ngOnInit() {
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]]
-    });
+    }); }
+
+  ngOnInit() {
   }
 
 }
