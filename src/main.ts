761251942bdc4455b94a1fc31147a73ac6751a13
arpho <damicogiuseppe77@gmail.com>
2024-11-23 17:46:42 +0100

    initialized firebase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,54 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-//TODO  understand how to list items by status: active|deleted
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-  
+import { environment } from './environments/environment';
+import { provideHttpClient } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { addCircle, add, addCircleOutline, removeCircle, remove, createOutline, trashOutline } from 'ionicons/icons';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
+import { reservedKeys } from './app/settings/reserved';
+if (environment.production) {
+  enableProdMode();
+}
+addIcons({
+  'add-circle': addCircle,
+  'add-circle-outline': addCircleOutline,
+  'add': add,
+  'remove-circle': removeCircle,
+  'remove': remove,
+  'create-outline': createOutline,
+  'trash-outline': trashOutline,
+});
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
+    importProvidersFrom(IonicModule.forRoot()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase }, provideFirebaseApp(() => initializeApp({"projectId":"m1crossfit-5b2b9","appId":"1:959204307943:web:24ed9f53f6b914d0608342","databaseURL":"https://m1crossfit-5b2b9.firebaseio.com","storageBucket":"m1crossfit-5b2b9.firebasestorage.app","apiKey":"AIzaSyDh_aGD_ksup-PasR7ewLjHjZ7qHjoSzgQ","authDomain":"m1crossfit-5b2b9.firebaseapp.com","messagingSenderId":"959204307943"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider(reservedKeys.recptchaSite);
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+}), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideVertexAI(() => getVertexAI()),
+  ],
+});
+defineCustomElements(window);
